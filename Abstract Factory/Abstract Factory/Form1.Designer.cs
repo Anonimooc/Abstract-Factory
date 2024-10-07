583db59b307d7d8f27fa193eeb9ad0806ae3a598
Zakusilo Denis <zakusilodr@mer.ci.nsu.ru>
2024-10-07 15:06:44 +0700

    Add files via upload

---
+++ Abstract Factory/Abstract Factory/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxShapes = new System.Windows.Forms.ComboBox();
+            this.comboBoxColors = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBoxShapes
+            // 
+            this.comboBoxShapes.FormattingEnabled = true;
+            this.comboBoxShapes.Location = new System.Drawing.Point(456, 39);
+            this.comboBoxShapes.Name = "comboBoxShapes";
+            this.comboBoxShapes.Size = new System.Drawing.Size(277, 21);
+            this.comboBoxShapes.TabIndex = 0;
+            this.comboBoxShapes.SelectedIndexChanged += new System.EventHandler(this.ComboBoxShapes_SelectedIndexChanged);
+            // 
+            // comboBoxColors
+            // 
+            this.comboBoxColors.FormattingEnabled = true;
+            this.comboBoxColors.Location = new System.Drawing.Point(69, 39);
+            this.comboBoxColors.Name = "comboBoxColors";
+            this.comboBoxColors.Size = new System.Drawing.Size(277, 21);
+            this.comboBoxColors.TabIndex = 1;
+            this.comboBoxColors.SelectedIndexChanged += new System.EventHandler(this.ComboBoxColors_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(69, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "Выберите цвет:";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(456, 11);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(89, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "Выберите фигуру:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBoxColors);
+            this.Controls.Add(this.comboBoxShapes);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxShapes;
+        private System.Windows.Forms.ComboBox comboBoxColors;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Abstract Factory/Abstract Factory/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class MainForm : Form
+    {
+        private IShapeFactory _currentFactory;
+        private IShape _currentShape;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            comboBoxShapes.SelectedIndexChanged += ComboBoxShapes_SelectedIndexChanged;
+            comboBoxColors.SelectedIndexChanged += ComboBoxColors_SelectedIndexChanged;
+
+            // Инициализация
+            comboBoxColors.Items.Add("Red");
+            comboBoxColors.Items.Add("Blue");
+            comboBoxShapes.Items.Add("Circle");
+            comboBoxShapes.Items.Add("Square");
+            comboBoxShapes.Items.Add("Triangle");
+
+            comboBoxColors.SelectedIndex = 0; // по умолчанию красный
+            comboBoxShapes.SelectedIndex = 0; // по умолчанию круг
+
+            UpdateFactory();
+            CreateShape();
+        }
+
+        private void ComboBoxColors_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateFactory();
+            CreateShape();
+            Invalidate(); // перерисовать форму
+        }
+
+        private void ComboBoxShapes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            CreateShape();
+            Invalidate(); // перерисовать форму
+        }
+
+        private void UpdateFactory()
+        {
+            string selectedColor = comboBoxColors.SelectedItem.ToString();
+            if (selectedColor == "Red")
+            {
+                _currentFactory = new RedFactory();
+            }
+            else
+            {
+                _currentFactory = new BlueFactory();
+            }
+        }
+
+        private void CreateShape()
+        {
+            // проверка, что выбран элемент в comboBoxShapes
+            if (comboBoxShapes.SelectedItem == null)
+            {
+                _currentShape = null;
+                return;
+            }
+
+            string selectedShape = comboBoxShapes.SelectedItem.ToString();
+
+            // проверка, что _currentFactory не равен null
+            if (_currentFactory == null)
+            {
+                _currentShape = null;
+                return;
+            }
+
+            switch (selectedShape)
+            {
+                case "Circle":
+                    _currentShape = _currentFactory.CreateCircle();
+                    break;
+                case "Square":
+                    _currentShape = _currentFactory.CreateSquare();
+                    break;
+                case "Triangle":
+                    _currentShape = _currentFactory.CreateTriangle();
+                    break;
+                default:
+                    _currentShape = null;
+                    break;
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            _currentShape?.Draw(e.Graphics); // рисуем текущую фигуру
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public interface IShape
+    {
+        void Draw(Graphics g);
+    }
+
+    public class Circle : IShape
+    {
+        public Color Color { get; set; }
+
+        public Circle(Color color)
+        {
+            Color = color;
+        }
+
+        public void Draw(Graphics g)
+        {
+            using (Brush brush = new SolidBrush(Color))
+            {
+                g.FillEllipse(brush, 150, 150, 100, 100);
+            }
+        }
+    }
+
+    public class Square : IShape
+    {
+        public Color Color { get; set; }
+
+        public Square(Color color)
+        {
+            Color = color;
+        }
+
+        public void Draw(Graphics g)
+        {
+            using (Brush brush = new SolidBrush(Color))
+            {
+                g.FillRectangle(brush, 150, 150, 100, 100);
+            }
+        }
+    }
+
+    public class Triangle : IShape
+    {
+        public Color Color { get; set; }
+
+        public Triangle(Color color)
+        {
+            Color = color;
+        }
+
+        public void Draw(Graphics g)
+        {
+            using (Brush brush = new SolidBrush(Color))
+            {
+                Point[] points = { new Point(120, 250), new Point(180, 150), new Point(240, 250) };
+                g.FillPolygon(brush, points);
+            }
+        }
+    }
+
+    public interface IShapeFactory
+    {
+        IShape CreateCircle();
+        IShape CreateSquare();
+        IShape CreateTriangle();
+    }
+
+    public class RedFactory : IShapeFactory
+    {
+        public IShape CreateCircle() => new Circle(Color.Red);
+        public IShape CreateSquare() => new Square(Color.Red);
+        public IShape CreateTriangle() => new Triangle(Color.Red);
+    }
+
+    public class BlueFactory : IShapeFactory
+    {
+        public IShape CreateCircle() => new Circle(Color.Blue);
+        public IShape CreateSquare() => new Square(Color.Blue);
+        public IShape CreateTriangle() => new Triangle(Color.Blue);
+    }
+}
